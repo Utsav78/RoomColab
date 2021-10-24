6f146191319da010c040efa5a52dece1eeae810c
Utsav <utsavbudathoki12@gmail.com>
2021-10-24 15:00:40 +0545

    initial commit

---
+++ app/src/main/java/com/example/roomcodelab/WordDao.kt
@@ -0,0 +1,21 @@
+package com.example.roomcodelab
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import java.util.concurrent.Flow
+
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): kotlinx.coroutines.flow.Flow<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
